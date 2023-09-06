914d1da0176c4fec198c1b82c5c0f97209bcfa00
PATEL KRISH <127637917+PATEL-KRISH-0@users.noreply.github.com>
2023-09-06 13:14:46 +0530

    Add files via upload

---
+++ TODOLIST/Source Code/app.js
@@ -0,0 +1,67 @@
+// THIS FUNCTION RUN FOR ADDING TODO
+function addtodo() {
+    let list_text = document.querySelector(".input-Text-Todo").value;
+    if (list_text == "") {
+        alert("Enter somthing");
+    } else {
+
+        let listTodo = document.querySelector(".list-Container-Ul")
+        listTodo.insertAdjacentHTML('beforeEnd',
+            `<li class="list-Itomes">
+            <div class="checkbox-And-List-Text-Container">
+                <input class="checkbox" onclick="checkCheckBox()" type="checkbox">
+                <span class="list-Itomes-Text">${list_text}</span>
+            </div>
+            <div class="edit-And-Delete-Btn-Container">
+                <button class="edit"  onclick="editTodo()">
+                    <svg viewBox="0 0 512 512">
+                        <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
+                            <title>Edit11</title>
+                    </svg>
+                </button>
+                <button class="delete" onclick="deleteTodo()">
+                    <svg viewBox="0 0 448 512">
+                        <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+                        <title>Delete</title>
+                    </svg>
+                </button>
+            </div>
+        </li>`
+        );
+    }
+    document.querySelector(".input-Text-Todo").value = null
+}
+
+// THIS FUNCTION RUN FOR DELETE TODO LIST ITOME
+function deleteTodo() {
+    let deleteed = event.target.closest(".list-Itomes");
+    deleteed.remove()
+}
+
+// THIS FUNCTION RUN FOR EDIT TODO TEXT
+function editTodo() {
+    let eTodo = event.target.closest(".list-Itomes");
+    console.log(eTodo);
+    if (eTodo) {
+        let listItemText = eTodo.querySelector(".list-Itomes-Text");
+        let newText = prompt("Enter new text");
+        if (newText !== null) {
+            listItemText.textContent = newText;
+        }
+    }
+}
+
+// THIS FUNCTION RUN FOR CHECK CHECK BOX CHEKED OR NOT 
+function checkCheckBox() {
+    let invalue = event.target.checked;
+    console.log(invalue);
+    if (invalue) {
+        let nextsib = event.target.nextElementSibling.innerHTML;
+        event.target.nextElementSibling.innerHTML = `<del>${nextsib}</del>`;
+        console.log(nextsib);
+    } else {
+        let nextsib = event.target.nextElementSibling.innerHTML;
+        event.target.nextElementSibling.innerHTML = nextsib.replace("<del>", "").replace("</del>", "");
+        console.log(nextsib);
+    }
+}
